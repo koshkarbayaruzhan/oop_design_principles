1c886a60582d5c2202dd186f0749c2668619e4af
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-01 20:56:14 +0500

    Update LoggingService.java

--- src/design/LoggingService.java
+++ src/design/LoggingService.java
@@ -1,4 +1,9 @@
 package design;
 
 public class LoggingService {
+
+    public void log(String message) {
+        String connectionString = Config.CONNECTION_STRING;
+        System.out.println("Logging to DB: " + message);
+    }
 }
