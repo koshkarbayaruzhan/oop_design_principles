e27be7c47f1b398aba8865c8281ccd01b620139c
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-01 20:55:38 +0500

    Update DatabaseService.java

--- src/design/DatabaseService.java
+++ src/design/DatabaseService.java
@@ -1,4 +1,9 @@
 package design;
 
 public class DatabaseService {
+
+    public void connect() {
+        String connectionString = Config.CONNECTION_STRING;
+        System.out.println("Connecting to DB: " + connectionString);
+    }
 }
