8a1e3520b762ec82b1d8d6a1548954ccdcd3f86e
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-01 20:58:39 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import design.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Logger.log("INFO", "Application started");
+
+        DatabaseService db = new DatabaseService();
+        db.connect();
+
+        KissExamples kiss = new KissExamples();
+        kiss.processNumbers(new int[]{1, -2, 3, 0});
+
+        User user = new User();
+        user.name = "Test";
+        user.email = "test@mail.com";
     }
 }
+
