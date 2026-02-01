fe5578a7fdaf488418c9e512304a3910b6aaa813
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-01 20:57:21 +0500

    Update KissExamples.java

--- src/design/KissExamples.java
+++ src/design/KissExamples.java
@@ -1,4 +1,19 @@
 package design;
 
 public class KissExamples {
+
+    public void processNumbers(int[] numbers) {
+        if (numbers == null) return;
+
+        for (int number : numbers) {
+            if (number > 0) {
+                System.out.println(number);
+            }
+        }
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) return 0;
+        return a / b;
+    }
 }
